03b6e53986ee9dcc4083091513d9d1a827c0de30
zhenliang.sun <solszl@163.com>
2022-10-17 02:06:54 +0800

    fix shallow clone error

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 import { Store, subscribe, subscribeKey } from "./src";
 
-const state = Store({ a: "1", b: "2", obj: { foo: "bar", b: "ooo", a: "yyy" } });
+const state = Store({
+  a: "1",
+  b: "2",
+  obj: { foo: "bar", b: "ooo", a: "yyy" },
+  obj2: { arr: [1, 2, 3] },
+});
 const unsubscribe = subscribe(state, (v) => {
   console.error("state changed", state, v);
 });
@@ -13,17 +18,12 @@ const unsubscribeKey2 = subscribeKey(state.obj, "b", (v) => {
   console.error("state changed by key2", state, v);
 });
 
+const unsubscribeKey3 = subscribeKey(state.obj2, "arr", (v) => {
+  console.error("state changed by key3", state, JSON.stringify(v));
+});
+
 setTimeout(() => {
-  state.obj.b = "111";
+  state.obj2 = {
+    arr: [2, 3, 4],
+  };
 }, 1000);
-
-setTimeout(() => {
-  console.log("unsubscribe");
-  unsubscribe();
-  unsubscribeKey();
-  unsubscribeKey2();
-}, 3000);
-
-setTimeout(() => {
-  state.a = "3";
-}, 4000);
--- src/utils.js
+++ src/utils.js
@@ -18,3 +18,20 @@ export const canProxy = (obj) => {
 };
 
 export const INTERNAL_LISTENERS = "__LISTENER__";
+
+export const deepClone = (value) => {
+  if (!(!!value && typeof value == "object")) {
+    return value;
+  }
+  if (Object.prototype.toString.call(value) == "[object Date]") {
+    return new Date(value.getTime());
+  }
+  if (Array.isArray(value)) {
+    return value.map(deepClone);
+  }
+  var result = {};
+  Object.keys(value).forEach(function (key) {
+    result[key] = deepClone(value[key]);
+  });
+  return result;
+};
